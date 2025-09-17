aec769149e9fcc00f378b057652b1a9ff4f26aa6
MJ1605 <88710224+MJ1605@users.noreply.github.com>
2025-09-17 11:37:15 +0800

    goal setting ui somewhat

--- app/routes/dashboard/kpi.tsx
+++ app/routes/dashboard/kpi.tsx
@@ -1,4 +1,7 @@
+import { FaEdit } from "react-icons/fa";
 import type { Route } from "./+types/settings";
+import { Button, Dialog, DialogContent, DialogTitle, IconButton } from "@mui/material";
+import { useState } from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -8,11 +11,32 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function KPIPage() {
+  const [goalModalOpen, SetGoalModalOpen] = useState(false);
+
+
   return (
     <div className="h-screen w-full">
       <div className="flex h-1/6 w-full flex-col">
-        <div className="flex h-1/3 bg-amber-400 text-4xl p-2">KPI</div>
-        <div className="flex h-2/3 bg-orange-700"></div>
+        <div className="flex h-1/3 bg-amber-400 text-4xl p-2 font-bold">KPI</div>
+        <div className="flex h-2/3 bg-neutral-800 m-2 rounded-3xl p-2">
+          <div className="flex w-1/2 flex-col">
+            <p className="text-3xl font-bold">Goal for this month:</p>
+            <div className="flex flex-row gap-2 items-center">
+              <p>Testing</p>
+              <FaEdit className="hover:underline cursor-pointer" onClick={()=> SetGoalModalOpen(true)}/>
+              <Dialog open={goalModalOpen} onClose={()=> SetGoalModalOpen(false)}>
+                <DialogTitle>Set Your Goals</DialogTitle>
+                <DialogContent>
+                  <p>This adna ougba oifbasb dfoasblf uoaw builfbaw ilfasdbvcjba slfbjas fblwbek fdf</p>
+                  <div className="flex justify-end">
+                    <Button>Save</Button>
+                    <Button onClick={()=>SetGoalModalOpen(false)}>Close</Button>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="flex h-5/6 w-full">
         <div className="flex w-1/2 h-full bg-blue-400"></div>
--- app/themes.tsx
+++ app/themes.tsx
@@ -11,4 +11,15 @@ export const darktheme = createTheme({
       paper: "#1e1e1e",
     },
   },
+  components: {
+    MuiDialog: {
+      styleOverrides: {
+        paper: {
+          borderRadius: "1rem",
+          backgroundColor: "#212121",
+          padding: "1rem",
+        },
+      },
+    },
+  },
 });
